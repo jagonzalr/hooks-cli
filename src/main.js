4ba57238ed55fa8fb6ec11bd277083b3b4912c67
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2020-04-28 18:09:54 +0200

    Copy template files

--- src/main.js
+++ src/main.js
@@ -9,13 +9,35 @@ import { promisify } from 'util'
 const access = promisify(fs.access)
 const copy = promisify(ncp)
 
+async function copyTemplateFiles(options) {
+	return copy(options.templateDirectory, options.targetDirectory, {
+  	clobber: false
+ 	})
+}
+
 export async function createProject(options) {
 	options = {
 	  ...options,
 	  targetDirectory: options.targetDirectory || process.cwd()
 	}
 
-	console.log(options.targetDirectory)
+	const currentFileUrl = import.meta.url;
+	const templateDir = path.resolve(
+	  new URL(currentFileUrl).pathname,
+	  '../../template'
+	)
+
+	options.templateDirectory = templateDir
+
+	try {
+  	await access(templateDir, fs.constants.R_OK)
+ 	} catch (err) {
+  	console.error('%s Invalid template path', chalk.red.bold('ERROR'))
+   	process.exit(1)
+ 	}
+
+ 	console.log('Copy project files')
+ 	await copyTemplateFiles(options)
 	console.log('%s Project ready', chalk.green.bold('DONE'));
 	return true
 }
